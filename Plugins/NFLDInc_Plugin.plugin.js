2cfdfd509ca1ce0514a4d219ef9dcb9c93a2bb1d
Dawson <NFLD99@users.noreply.github.com>
2020-06-07 03:49:38 -0230

    Updated

--- Plugins/NFLDInc_Plugin.plugin.js
+++ Plugins/NFLDInc_Plugin.plugin.js
@@ -3,9 +3,9 @@
 class NFLDInc_Plugin {
   getName() { return "NFLDInc_Plugin"; }
 
-  getVersion() { return "6.0.0"; }
+  getVersion() { return "7.0.0"; }
 
-  getAuthor() { return "Nfld99"; }
+  getAuthor() { return "NFLD99"; }
 
   getDescription() { return "NFLDInc_Plugin."; }
 
@@ -15,7 +15,7 @@ class NFLDInc_Plugin {
 
   start() {
     // If ZLib exists, check for updates
-    if (window.ZLibrary) ZLibrary.PluginUpdater.checkForUpdate("NFLDInc_Plugin","6.0.0","https://raw.githubusercontent.com/NFLD99/Better-Discord/master/Plugins/NFLDInc_Plugin.plugin.js")
+    if (window.ZLibrary) ZLibrary.PluginUpdater.checkForUpdate("NFLDInc_Plugin", this.getVersion(),"https://raw.githubusercontent.com/NFLD99/Better-Discord/master/Plugins/NFLDInc_Plugin.plugin.js")
 
     const UserStore = BdApi.findModuleByProps("getCurrentUser");
 
@@ -25,6 +25,7 @@ class NFLDInc_Plugin {
   }
 
   addElement(avatarElement, id) {
+	if (avatarElement.parentElement.querySelector(".NFLDIncChatUser")) return;
     const newSibling = document.createElement("div");
     newSibling.classList.add("NFLDIncChatUser");
     newSibling.setAttribute("NFLDIncChatUser", id ? id : avatarElement.src.split("/")[4]);
@@ -38,7 +39,7 @@ class NFLDInc_Plugin {
     const avatarInner = added.querySelectorAll(".avatar-VxgULZ");
     if (avatarInner.length) {
         // modal user
-        if (avatarInner.length == 1) { 
+        if (avatarInner.length == 1) {
             const id = avatarInner[0].src.split("/")[4];
             const modal = avatarInner[0].closest(".inner-1ilYF7");
             if (modal) return modal.setAttribute("NFLDIncPopoutUser", id);
@@ -46,7 +47,8 @@ class NFLDInc_Plugin {
 
         // For DevilBro or StatusEverywhere
         for (let i = 0; i < avatarInner.length; i++) {
-            const avatarOuter = avatarInner[i].closest(".avatar-1BDn8e");
+			// chat class, member list class
+            const avatarOuter = avatarInner[i].closest(".avatar-1BDn8e, .avatar-3uk_u9");
             if (avatarOuter) this.addElement(avatarOuter, avatarInner[i].src.split("/")[4]);
         }
         return; // To avoid the stuff below
@@ -57,24 +59,15 @@ class NFLDInc_Plugin {
     if (avatarOuter.length) {
         for (let i = 0; i < avatarOuter.length; i++) this.addElement(avatarOuter[i]);
     }
-  }
-
-  observer(mutation) {
-    // If XenoLib exists, check for toasts and sent them to notifactions
-    if (window.XenoLib) {
-    const Toast = document.querySelector(".toast-text");
-    if (Toast !== null) {
-    Toast.style.display = "none";
-    XenoLib.Notifications.danger(Toast.textContent, {timeout: 3000});
-    }
-    }
-    if (window.XenoLib) {
-    const bdToast = document.querySelector(".bd-toast");
-    if (bdToast !== null) {
-    bdToast.style.display = "none";
-    XenoLib.Notifications.danger(bdToast.textContent, {timeout: 3000});
-    }
-    }
+	
+	// Do the toasts
+	if (window.XenoLib) {
+		const toast = added.querySelector(".toast-text") || added.querySelector(".bd-toast");
+		if (toast) {
+			toast.style.display = "none";
+			XenoLib.Notifications.danger(toast.textContent, {timeout: 3000});
+		}
+	}
   }
 
   stop() {
